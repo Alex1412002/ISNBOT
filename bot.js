920ac1cdb32eef568e5730dcefa2b6506a8bcf67
Alex1412002 <58090762+Alex1412002@users.noreply.github.com>
2019-11-23 22:13:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
 client.on('ready', () => {
     console.log('pret a casser des culs');
 });
-
 client.on('message', message => {
     if (message.content === 'ping') {
         message.reply('pong');
     }
 });
-
 client.login(process.env.BOT_TOKEN);
