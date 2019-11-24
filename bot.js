42f8a28230b452a6cd384ab90e89a2bf80bc9199
Alex1412002 <58090762+Alex1412002@users.noreply.github.com>
2019-11-24 10:01:02 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,4 +8,21 @@ client.on('message', message => {
         message.reply('pong');
     }
 });
+bot.on('message', msg =>{
+    if(msg.content.match(/salut/i)){
+        msg.channel.send("Cc")
+    }
+    if(msg.content.match(/cc/i)){
+        msg.channel.send("comment allez vous aujourd'hui ?")
+    }
+    if(msg.content.match(/wsh/i)){
+        msg.channel.send("Yo le frero")
+    }
+    if(msg.content.match(/slt/i)){
+        msg.channel.send("Coucou chérie")
+    }
+    if(msg.content.match(/bonjour/i)){
+        msg.channel.send("hey")
+    }
+});
 client.login(process.env.BOT_TOKEN);
