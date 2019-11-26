4fbb74c4e0119377e59e92e02b3236f2c4bbace6
Alex1412002 <58090762+Alex1412002@users.noreply.github.com>
2019-11-26 19:20:43 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,27 +10,27 @@ client.on('message', message => {
 });
 client.on('message', message =>{
     if(message.content.match(/salut/i)){
-        message.channel.send("Cc")
+        message.channel.send("Cc");
     }
     if(message.content.match(/cc/i)){
-        message.channel.send("comment allez vous aujourd'hui ?")
+        message.channel.send("comment allez vous aujourd'hui ?");
     }
     if(message.content.match(/wsh/i)){
-        message.channel.send("Yo le frero")
+        message.channel.send("Yo le frero");
     }
     if(message.content.match(/slt/i)){
-        message.channel.send("Coucou chérie")
+        message.channel.send("Coucou chérie");
     }
     if(message.content.match(/bonjour/i)){
-       message.channel.send("hey")
+       message.channel.send("hey");
     }
     if(message.content === '.gregoire') {
-        message.channel.send("Au secours ! Nous sommes Envahis. 3 créatures de type gregoire, peut ergonomique par dessus le marché sont ici. Fuyez !!!")
+        message.channel.send("Au secours ! Nous sommes Envahis. 3 créatures de type gregoire, peut ergonomique par dessus le marché sont ici. Fuyez !!!");
     }
 });
 client.on(‘message’, (receivedMessage) => {
     If (receivedMessage.author === ‘alex’) {
-        message.channel.send(‘ta gueule’)
+        message.channel.send(‘ta gueule’);
     }
 });
 client.login(process.env.BOT_TOKEN);
