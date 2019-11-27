81e6673d99deb4eb5ea42867d1b4b75feb14197d
Alex1412002 <58090762+Alex1412002@users.noreply.github.com>
2019-11-27 11:04:16 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ client.on('message', message =>{
     }
 });
 client.on('message', message  => {
-     if(message.author.tag === 'alex1412002#9761' ) {
+     if(message.author.tag === 'IRF alex1412002#9761' ) {
         message.channel.send('ta gueule')     
      }
 });
