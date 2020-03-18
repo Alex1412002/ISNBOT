19aba02fc35522ffb79b467e841b2175453da9b4
Alex1412002 <58090762+Alex1412002@users.noreply.github.com>
2020-03-18 14:58:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
 client.on('message', message  =>{
     if(message.content.match(/assistant !/i)){
-       message.channel.send("A votre service" + message.author.tag);
+       message.channel.send("A votre service " + message.author);
     }
 });
 
