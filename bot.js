abcafad13c99f12e09c7c2dca9af00817cdda505
Alex1412002 <58090762+Alex1412002@users.noreply.github.com>
2020-03-18 11:51:30 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,54 +9,8 @@ client.on('message', message => {
     }
 });
 client.on('message', message =>{
-    if(message.content.match(/salut/i)){
-        message.channel.send("Cc");
-    }
-    if(message.content.match(/cc/i)){
-        message.channel.send("comment allez vous aujourd'hui ?");
-    }
-    if(message.content.match(/wsh/i)){
-        message.channel.send("Yo le frero");
-    }
-    if(message.content.match(/slt/i)){
-        message.channel.send("Coucou chérie");
-    }
-    if(message.content.match(/bonjour/i)){
-       message.channel.send("hey");
-    }
-    if(message.content.match(/bite/i)){
-       message.channel.send("vocabulaire !!!");
-    }
-    if(message.content === '.gregoire') {
-        message.channel.send("Au secours ! Nous sommes Envahis. 3 créatures de type gregoire, peut ergonomique par dessus le marché sont ici. Fuyez !!!");
-    }
-    if(message.content.match(/putin/i)){
-       message.channel.send("vocabulaire !!!");
-    }
-    if(message.content.match(/merde/i)){
-       message.channel.send("vocabulaire !!!");
-    }
-    if(message.content.match(/ chier/i)){
-       message.channel.send("vocabulaire !!!");
-    }
-    if(message.content.match(/ nique/i)){
-       message.channel.send("vocabulaire !!!");
-    }
-    if(message.content.match(/enfoiré/i)){
-       message.channel.send("vocabulaire !!!");
-    }
-    if(message.content.match(/ couilles/i)){
-       message.channel.send("vocabulaire !!!");
-    }
     if(message.content.match(/ pute/i)){
        message.channel.send("vocabulaire !!!");
-    }
-    
-    if(message.content.match(/sah/i)){
-       message.channel.send("pouvais vous parler francais s'il vous plait ?");
-    }
-    if(message.content.match(/stephanie/i)){
-       message.channel.send("la gentille steph pour les intime");
     }
 });
 client.on('message', message  => {
