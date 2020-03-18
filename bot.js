f667057912994a722e49df4df938b3e8aa5f08e2
Alex1412002 <58090762+Alex1412002@users.noreply.github.com>
2020-03-18 14:08:47 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-client.on('ready', () => {
-    console.log('pret a casser des culs');
-});
+
 client.on('message', message => {
     if (message.content === 'ping') {
         message.channel.send('pong');
