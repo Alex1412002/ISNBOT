6930235e4dc2b37f107d0753a8f8a78976b0c87c
Alex1412002 <58090762+Alex1412002@users.noreply.github.com>
2020-03-24 20:39:27 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -52,7 +52,9 @@ client.on('message', message =>{
        message.channel.send(":small_blue_diamond: pour une bonne boutade il faut ecrire *raconte moi une blague* \n:small_blue_diamond: pour avoir mon avis sur l'invasion du luxembourg : *!envahisleluxembourg* \n:small_blue_diamond: Et puis jsuis pas a tes ordres donc je l'ouvre quand je veux");
     }
     if(message.content.match(/!envahisleluxembourg/i)){
-       message.channel.send("je suis pacifique en revanche je frappe vite");
+        if(message.author.tag != 'ISN bot#5674'){
+            message.channel.send("je suis pacifique en revanche je frappe vite");
+        }
     }
 
     if((message.content.match(/<@!647166866329960462> !/i))||(message.content.match(/<@647166866329960462> !/i))){
