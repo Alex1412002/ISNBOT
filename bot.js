dae8defca8bbc01cf05328614bb0a52a7a4f639c
Alex1412002 <58090762+Alex1412002@users.noreply.github.com>
2020-04-04 22:28:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,7 +58,7 @@ client.on('message', message =>{
        message.channel.send(":small_blue_diamond: pour une bonne boutade il faut ecrire *raconte moi une blague* \n:small_blue_diamond: pour avoir mon avis sur l'invasion du luxembourg : *!envahisleluxembourg* \n:small_blue_diamond: Et puis jsuis pas a tes ordres donc je l'ouvre quand je veux");
     }
     if(message.content.match(/<@&689041074538283008>/i)){
-       if(auteur != 'ISN bot#5674' && auteur != 'if(auteur === 'Alex1412002#9761') {
+       if(auteur != 'ISN bot#5674' && auteur != 'Alex1412002#9761') {
            message.channel.send("T'as bien réfléchi avant de t'adresser au Dieu ?");
        }
     }
