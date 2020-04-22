45a2c79e712ee69b67268e7d05e2236194990ffb
Alex1412002 <58090762+Alex1412002@users.noreply.github.com>
2020-04-22 21:24:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -162,10 +162,7 @@ client.on('message', message =>{
     
 /*--------------------------------------------------------*/
     if(auteur != 'ISN bot#5674'){ 
-        if(message.content.match(/!DJadd/i)) {
-            message.channel.send("on va essayer")
-            
-        }
+ 
         /*presentation du bot*/
         if(message.content.match(/!presentation/i)) {
               message.channel.send("Bonjour, Je suis l'assistant ! j'ai été concu par **dieu** et par l'**Aassistant de l'Assistant**. Bienvenue sur l'**autoroute du rire**. \nPour me presenter or to introduce myself (sa fait stylé en anglais vous trouvez pas ? :thinking:) voici mes differentes qualités:\nPour avoir un rappel des commandes : `!presentation`\nPour une bonne boutade : `@assistant raconte moi une blague`\nPour verifier que je suis present : `!statut`\nSans compter mes nombreuses interventions qui animeront vivement les discussions.\nVoila on rigole on rigole mais on voit pas le fond du bol. N'oubliez pas : RESTEZ CHEZ VOUS");
